5ce158e775aae772e77f4ef58c33fb3579a9a76c
Apriyadi Hegarto <ahegarto@gmail.com>
2022-12-30 21:08:43 +0700

    Test alert

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 
 const io = new Server(server, {
   cors: {
-    origin: "https://tbheroesserver.vercel.app/",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
