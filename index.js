affc74579602971693df440e450e5fbfb9fe057a
Apriyadi Hegarto <ahegarto@gmail.com>
2022-12-30 19:16:13 +0700

    test deploy

--- index.js
+++ index.js
@@ -8,7 +8,44 @@ app.get("/", (req, res) => {
   res.send("Testing tersambung ke server");
 });
 
-app.use("/", express.json(), router);
+// app.use("/", express.json(), router);
+
+// const http = require("http");
+// const cors = require("cors");
+// const { Server } = require("socket.io");
+// app.use(cors());
+
+// const server = http.createServer(app);
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST"],
+//   },
+// });
+
+// io.on("connection", (socket) => {
+//   console.log(`User Connected: ${socket.id}`);
+
+//   socket.on("join_room", (data) => {
+//     socket.join(data);
+//     console.log(`User with ID: ${socket.id} joined room: ${data}`);
+//   });
+
+//   socket.on("send_message", (data) => {
+//     socket.to(data.room).emit("receive_message", data);
+//   });
+
+//   socket.on("disconnect", () => {
+//     console.log("User Disconnected", socket.id);
+//   });
+// });
+
+// server.listen(3001, () => {
+//   console.log("SERVER RUNNING");
+// });
+
+
 
 app.listen(port, () => {
   console.log(port, "Server is open with port!");
