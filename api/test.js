c07f55ac0e85e69e8b1be5f1b432f638ff22fe5e
Apriyadi Hegarto <ahegarto@gmail.com>
2023-01-17 13:42:40 +0700

    tambah auth middleware

--- api/test.js
+++ api/test.js
@@ -3,16 +3,16 @@ const express = require("express");
 const router = express.Router();
 
 // const adminMiddleware = require("../middlewares/adminMiddleware");
-// const authMiddleware = require("../middlewares/authMiddleware");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 router.get("/test/test", (req, res) => {
   res.send("Connected to API test");
 });
 
-// // authMiddleware Tester
-// router.get("/schemas/authmiddleware", authMiddleware, (req, res) => {
-//   res.send("Connected to API tester schema with auth");
-// });
+// authMiddleware Tester
+router.get("/schemas/authmiddleware", authMiddleware, (req, res) => {
+  res.send("Connected to API tester schema with auth");
+});
 // // adminMiddleware Tester
 // router.get("/schemas/adminmiddleware", adminMiddleware, (req, res) => {
 //   res.send("Connected to API tester schema with admin");
