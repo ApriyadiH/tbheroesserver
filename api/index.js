0d25e3a59629567eeffec0a8e46dac4d6d149a3a
Apriyadi Hegarto <ahegarto@gmail.com>
2023-01-17 13:32:09 +0700

    hanya tester

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 // const chat = require("./chat");
 // const donor = require("./donor");
 // const event = require("./event");
-const login = require("./login");
+// const login = require("./login");
 // const marker = require("./marker");
-const register = require("./register");
+// const register = require("./register");
 // const request = require("./request");
 // const user = require("./user");
 // const schematester = require("./schematester");
