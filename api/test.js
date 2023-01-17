e315bfffd386f4e6377a584c12e868bda8d02869
Apriyadi Hegarto <ahegarto@gmail.com>
2023-01-17 13:41:17 +0700

    kurang middlewares

--- api/test.js
+++ api/test.js
@@ -2,21 +2,20 @@ const express = require("express");
 
 const router = express.Router();
 
-
-const adminMiddleware = require("../middlewares/adminMiddleware");
-const authMiddleware = require("../middlewares/authMiddleware");
+// const adminMiddleware = require("../middlewares/adminMiddleware");
+// const authMiddleware = require("../middlewares/authMiddleware");
 
 router.get("/test/test", (req, res) => {
   res.send("Connected to API test");
 });
 
-// authMiddleware Tester
-router.get("/schemas/authmiddleware", authMiddleware, (req, res) => {
-  res.send("Connected to API tester schema with auth");
-});
-// adminMiddleware Tester
-router.get("/schemas/adminmiddleware", adminMiddleware, (req, res) => {
-  res.send("Connected to API tester schema with admin");
-});
+// // authMiddleware Tester
+// router.get("/schemas/authmiddleware", authMiddleware, (req, res) => {
+//   res.send("Connected to API tester schema with auth");
+// });
+// // adminMiddleware Tester
+// router.get("/schemas/adminmiddleware", adminMiddleware, (req, res) => {
+//   res.send("Connected to API tester schema with admin");
+// });
 
 module.exports = router;
